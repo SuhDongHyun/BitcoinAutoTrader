ed0d1e36226c3e878e289e526380058bd49e72a0
Suh DongHyun <adamsuh008@gmail.com>
2025-04-02 19:52:44 +0900

    UpbitApi 가격/트랜드 조회 함수 추가

--- api/upbit_api.py
+++ api/upbit_api.py
@@ -17,3 +17,12 @@ class UpbitApi:
         self.secret_key = "tZsYxNv98LIdCxqPTLBRypcEvGDV3jjkBtFM0Ifh"
         self.upbit = pyupbit.Upbit(self.access_key, self.secret_key)
         self._initialized = True
+
+    # noinspection PyMethodMayBeStatic
+    def get_current_price(self, ticker="KRW-BTC"):
+        return pyupbit.get_current_price(ticker)
+
+    # noinspection PyMethodMayBeStatic
+    def get_price_trend(self, ticker="KRW-BTC", interval="day", count=200, to=None):
+        return pyupbit.get_ohlcv(ticker, interval, count, to)
+    
