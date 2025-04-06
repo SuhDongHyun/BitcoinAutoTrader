dfe17f77d9f09d13f090d8fd51e017955efc97e1
Suh DongHyun <adamsuh008@gmail.com>
2025-04-06 15:08:54 +0900

    UpbitApi Singleton 패턴 직접 구현 간소화

--- api/upbit_api.py
+++ api/upbit_api.py
@@ -1,22 +1,13 @@
+from util.singleton import singleton
 import pyupbit
 
 
+@singleton
 class UpbitApi:
-    _instance = None
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls._instance._initialized = False
-        return cls._instance
-
     def __init__(self):
-        if self._initialized:
-            return
         self.access_key = "zry4vxghMixsVamXLyibZxJnR3IhKQ4C0GzUCwT8"
         self.secret_key = "tZsYxNv98LIdCxqPTLBRypcEvGDV3jjkBtFM0Ifh"
         self.upbit = pyupbit.Upbit(self.access_key, self.secret_key)
-        self._initialized = True
 
     # noinspection PyMethodMayBeStatic
     def get_current_price(self, ticker="KRW-BTC"):
