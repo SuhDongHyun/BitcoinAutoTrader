94fb8b1d234ac32628d24017291f62855620d17f
Suh DongHyun <adamsuh008@gmail.com>
2025-04-07 22:02:21 +0900

    NewsSentiment 뉴스 제목 조회 함수 추가

--- strategy/news_sentiment.py
+++ strategy/news_sentiment.py
@@ -1,7 +1,13 @@
 from util.singleton import singleton
+import feedparser
 
 
 @singleton
 class NewsSentiment:
     def __init__(self):
         self.url = "https://news.google.com/rss/search?q=query+when:1d&hl=en&gl=US&ceid=US:en"
+
+    def get_news(self, query="economy", max_items=100):
+        feed = feedparser.parse(self.url.replace('query', query))
+        news_items = [entry['title'] for entry in feed['entries'][:max_items]]
+        return news_items
