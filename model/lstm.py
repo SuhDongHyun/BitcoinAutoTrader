e8e611d7c3380204bf936e48c8ce6e5ef6caf19c
Suh DongHyun <adamsuh008@gmail.com>
2025-04-13 15:49:38 +0900

    LSTM 모델 Class 생성

---
+++ model/lstm.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(LSTM, self).__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        lstm_out, _ = self.lstm(x)
+        linear_out = self.linear(lstm_out[:, -1, :])
+        return linear_out
